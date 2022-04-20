e539f7a2dc20f3f409ff650068624fd20f0add27
Adriano Goto <gotoadriano8@gmail.com>
2022-04-20 08:26:18 -0300

    Alterações nos arquivos de dados do silenciador

--- Data_Silencer.m
+++ Data_Silencer.m
@@ -1,23 +1,27 @@
             %%%%% ---------- Data_Silencer ---------- %%%%%
 % =============================================================================== % 
-% Acoustical silencer parameters
+% Acoustical silencer parameters based from from A. Selamet (1997) - "The effect 
+% of length on the acoustic attenuation perfomance of concentric expansion 
+% chambers"
+% Acoustical silencer parameters for the manufactured ECM
 % =============================================================================== %
 
 %%%%% ----- Frequency range ----- %%%%%   
-    fo   = 1;       % Initial frequency [Hz]
-    fmax = 3e3;     % Final frequency [Hz]
-    df = 2.5;         % Frequency step [Hz]
-    freq = fo:df:fmax; 
-    omega = 2*pi*freq;
+    fo   = 1;           % Initial frequency [Hz]
+    fmax = 3e3;         % Final frequency [Hz]
+    df = 2.5;           % Frequency step [Hz]
+    freq = fo:df:fmax;  % Frequency vector [Hz]
+    omega = 2*pi*freq;  % Angular frequency vector [rad/s]
 
 %%%%% ----- Properties ----- %%%%%
-    eta = 1.8e-5;   % Viscosidade do ar [Pa*s]
+    eta = 1.8e-5;   % Air viscosity [Pa*s]
     rho = 1.204;    % Air density [kg/m^3]
     co  = 343.3;    % Sound speed in air [m/s]
        
 %%%%% ----- Acoustical silencer parameters ----- %%%%%    
     Lc = 104e-3;       % Chamber length [m]
-    Ld = 77.5e-3;
+    Ld = 77.5e-3;      % Inlet/outlet duct length
+    
     r1 = 53e-3/2;   
     r2 = 150e-3/2;   
     Sc = pi*r2^2;
--- Data_Silencer2.m
+++ Data_Silencer2.m
@@ -1,33 +1,32 @@
             %%%%% ---------- Data_Silencer ---------- %%%%%
 % =============================================================================== % 
-% Acoustical silencer parameters taken from from A. Selamet (199) - ""
+% Acoustical silencer parameters for the manufactured ECM
 % =============================================================================== %
 
 %%%%% ----- Frequency range ----- %%%%%   
-    fo   = 1;       % Initial frequency [Hz]
-    fmax = 3.3e3;     % Final frequency [Hz]
-    df = 25;         % Frequency step [Hz]
+    fo   = 1;           % Initial frequency [Hz]
+    fmax = 3.3e3;       % Final frequency [Hz]
+    df = 25;            % Frequency step [Hz]
     freq = fo:df:fmax; 
     omega = 2*pi*freq;
 
 %%%%% ----- Properties ----- %%%%%
-    eta = 1.8e-5;   % Viscosidade do ar [Pa*s]
+    eta = 1.8e-5;   % Air viscosity [Pa*s]
     rho = 1.204;    % Air density [kg/m^3]
     co  = 343.3;    % Sound speed in air [m/s]
        
 %%%%% ----- Acoustical silencer parameters ----- %%%%%    
     Lc = 28.23e-2;   % Chamber length [m]
-    Ld = 15e-2;
-    d = 60e-3;      % Inlet duct diameter [m]
-    Dc = 150e-3;    % Expansion chamber diameter [m]
-    r1 = d/2;       % Inlet duct radius [m]
-    r2 = Dc/2;      % Inner perforated duct radius [m]
-    Sc = pi*r2^2;
-    Sd = pi*r1^2;
-    Y = (rho*co)/Sd;
-    Nc =1;          % Number of celss
-    h = 2*Ld+Lc;
-    m=(r2/r1)^2;
+    Ld = 15e-2;      % Inlet/outlet duct length
+    Dd = 60e-3;      % Inlet/outlet duct diameter [m]
+    Dc = 150e-3;     % Expansion chamber diameter [m]
+    rd = Dd/2;       % Inlet duct radius [m]
+    rc = Dc/2;       % Inner perforated duct radius [m]
+    Sc = pi*r2^2;    % Transversal chamber area [m^2]
+    Sd = pi*r1^2;    % Transversal duct area [m^2]
+    Y = (rho*co)/Sd; % Volume velocity impedance [Rays/m^2]
+    Nc =1;           % Number of cells
+    h = 2*Ld+Lc;     % Length of the cell [m]     
     
 
 
